910d3231c58a2a68918de015b312b3360245b635
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-18 16:16:10 -0300

    Main.js modifying the code

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import List from '../List/List';
 import './Main.css';
 
 const initialState = {
@@ -11,6 +12,14 @@ export default class Main extends React.Component {
     list: [],
   }
 
+  componentDidMount() {
+    let list = localStorage.getItem('list');
+    if (list !== null) {
+      list = list.split(',');
+      this.setState({ list });
+    }
+  }
+
   handleChange = (e) => {
     this.setState({
       newList: e.target.value,
@@ -19,12 +28,13 @@ export default class Main extends React.Component {
 
   handleClick = () => {
     const list = [...this.state.list];
-    if (this.state.newList.length > 3) {
+    if (this.state.newList.length > 2) {
       list.push(this.state.newList);
       console.log(list);
       console.log('State:', this.state.list);
       this.setState({ list, newList: initialState.newList });
     }
+    localStorage.setItem('list', list);
   }
 
   render() {
@@ -33,8 +43,14 @@ export default class Main extends React.Component {
         <h1>Lista de Tarefas</h1>
         <form action="#">
           <input type="text" onChange={this.handleChange} value={this.state.newList} />
-          <button type="submit" onClick={this.handleClick}>Adicionar</button>
+          <button type="submit" onClick={this.handleClick}>
+            <i className="fa fa-plus" />
+            {' '}
+            Adicionar
+          </button>
         </form>
+        <hr />
+        <List list={this.state.list} />
       </div>
     );
   }
