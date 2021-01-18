bf27619af333ac91182db32178abc66c33efb12b
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-18 18:43:40 -0300

    Button Style

--- src/components/List/Button/Button.js
+++ src/components/List/Button/Button.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Button.css';
 
 export default (props) => (
-  <button type="button" className="trash">
-    <i className="fa fa-trash" />
+  <button type="button" className={props.icon} onClick={(e) => props.func(e, props.index)}>
+    <i className={`fa fa-${props.icon}`} />
   </button>
 );
