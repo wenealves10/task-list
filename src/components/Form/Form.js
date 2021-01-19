ce73c838a498cddbea271ec5dd9bb525ea3228df
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-19 07:27:33 -0300

    Components Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Form.css';
 
-export default (props) => (
-  <form action="#">
-    <input type="text" onChange={props.func.handleChange} />
-    <button type="submit">Enviar</button>
-  </form>
-);
+export default function Form({ handleChange, handleClick, newList }) {
+  return (
+    <form action="#">
+      <input type="text" onChange={handleChange} value={newList} />
+      <button type="submit" onClick={handleClick}>
+        <i className="fa fa-plus" />
+        {' '}
+        Adicionar
+      </button>
+    </form>
+  );
+}
+
+Form.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  handleClick: PropTypes.func.isRequired,
+  newList: PropTypes.string.isRequired,
+};
